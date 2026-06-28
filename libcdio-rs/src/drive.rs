322015c4d683e22cdb1290860a7c112924ada5bc
Shiva Kiran Koninty <shiva@skran.xyz>
2026-06-28 17:04:55 +0530

    libcdio-rs/drive: Read, Write and Misc capabilities

--- libcdio-rs/src/drive.rs
+++ libcdio-rs/src/drive.rs
@@ -19,10 +19,12 @@
 
 use std::{
     ffi::{CStr, CString, NulError, OsString},
+    fmt,
     mem::MaybeUninit,
     path::PathBuf,
 };
 
+use bitflags::bitflags;
 use displaydoc::Display;
 use libcdio_sys::cdio_hwinfo_t;
 use thiserror::Error;
@@ -129,6 +131,28 @@ impl Drive {
             })
         }
     }
+
+    /// Get the drive capabilities.
+    ///
+    /// # Errors
+    /// If the operation errored, or the drive is not available.
+    pub fn capabilities(&self) -> Result<DriveCapabilities, DriveOperationError> {
+        let mut read = 0;
+        let mut write = 0;
+        let mut misc = 0;
+        unsafe {
+            libcdio_sys::cdio_get_drive_cap(self.cdio.as_ptr(), &mut read, &mut write, &mut misc);
+        }
+
+        (|| {
+            Some(DriveCapabilities {
+                read: ReadCapabilities::from_bits(read)?,
+                write: WriteCapabilities::from_bits(write)?,
+                misc: MiscCapabilities::from_bits(misc)?,
+            })
+        })()
+        .ok_or(DriveOperationError)
+    }
 }
 
 /// could not find any drives
@@ -164,6 +188,152 @@ pub struct HardwareInfo {
     pub revision: String,
 }
 
+/// Drive capabilities
+#[derive(Clone, Copy, Debug)]
+pub struct DriveCapabilities {
+    pub read: ReadCapabilities,
+    pub write: WriteCapabilities,
+    pub misc: MiscCapabilities,
+}
+bitflags! {
+    #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct MiscCapabilities: u32 {
+        /// Can close tray
+        const CloseTray = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_CLOSE_TRAY;
+        /// Can eject
+        const Eject = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_EJECT;
+        /// Can disable manual eject
+        const Lock = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_LOCK;
+        /// Can set drive speed
+        const SelectSpeed = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_SELECT_SPEED;
+        /// Can select juke-box disc
+        const SelectDisc = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_SELECT_DISC;
+        /// Can read multiple sessions
+        const MultiSession = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_MULTI_SESSION;
+        /// Can detect if media changed
+        const MediaChanged = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_MEDIA_CHANGED;
+        /// Can hard reset device
+        const Reset = libcdio_sys::cdio_drive_cap_misc_t_CDIO_DRIVE_CAP_MISC_RESET;
+    }
+}
+bitflags! {
+    /// Read capabilities of the drive
+    #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct ReadCapabilities: u32 {
+        /// Can play audio
+        const Audio = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_AUDIO;
+        /// Can read CD-DA
+        const CdDa = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_CD_DA;
+        /// Can read CD+G
+        const CdPlusG = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_CD_G;
+        /// Can read CD-R
+        const CdR = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_CD_R;
+        /// Can read CD-RW
+        const CdRw = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_CD_RW;
+        /// Can read DVD-R
+        const DvdR = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_DVD_R;
+        /// Can read DVD+R
+        const DvdPlusR = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_DVD_PR;
+        /// Can read DVD-RAM
+        const DvdRam = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_DVD_RAM;
+        /// Can read DVD-ROM
+        const DvdRom = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_DVD_ROM;
+        /// Can read DVD-RW
+        const DvdRw = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_DVD_RW;
+        /// Can read DVD+RW
+        const DvdPlusRw = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_DVD_RPW;
+        /// Can read C2 errors
+        const C2Errors = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_C2_ERRS;
+        /// Can read Mode 2 Form 1 (VCD)
+        const Mode2Form1 = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_MODE2_FORM1;
+        /// Can read Mode 2 Form 2 (VCD)
+        const Mode2Form2 = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_MODE2_FORM2;
+        /// Can read MCN
+        const Mcn = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_MCN;
+        /// Can read ISRC
+        const Isrc = libcdio_sys::cdio_drive_cap_read_t_CDIO_DRIVE_CAP_READ_ISRC;
+    }
+}
+bitflags! {
+    /// Write capabilities of the drive
+    #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct WriteCapabilities: u32 {
+        /// Can write CD-R
+        const CdR = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_CD_R;
+        /// Can write CD-RW
+        const CdRw = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_CD_RW;
+        /// Can write DVD-R
+        const DvdR = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_DVD_R;
+        /// Can write DVD+R
+        const DvdPlusR = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_DVD_PR;
+        /// Can write DVD-RAM
+        const DvdRam = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_DVD_RAM;
+        /// Can write DVD-RW
+        const DvdRw = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_DVD_RW;
+        /// Can write DVD+RW
+        const DvdPlusRw = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_DVD_RPW;
+        /// Can write MRW (Mount Rainier)
+        const Mrw = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_MT_RAINIER;
+        /// Can write using Burn proof
+        const BurnProof = libcdio_sys::cdio_drive_cap_write_t_CDIO_DRIVE_CAP_WRITE_BURN_PROOF;
+    }
+}
+
+impl fmt::Display for MiscCapabilities {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            MiscCapabilities::CloseTray => write!(f, "Close Tray"),
+            MiscCapabilities::Eject => write!(f, "Eject"),
+            MiscCapabilities::Lock => write!(f, "Lock"),
+            MiscCapabilities::SelectSpeed => write!(f, "Select Speed"),
+            MiscCapabilities::SelectDisc => write!(f, "Select Juke-box disc"),
+            MiscCapabilities::MultiSession => write!(f, "Multi Read Sessions"),
+            MiscCapabilities::MediaChanged => write!(f, "Media Change Detection"),
+            MiscCapabilities::Reset => write!(f, "Hard Reset"),
+            _ => write!(f, "Unknown"),
+        }
+    }
+}
+impl fmt::Display for ReadCapabilities {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            ReadCapabilities::Audio => write!(f, "Play Audio"),
+            ReadCapabilities::CdDa => write!(f, "CD-DA"),
+            ReadCapabilities::CdPlusG => write!(f, "CD+G"),
+            ReadCapabilities::CdR => write!(f, "CD-R"),
+            ReadCapabilities::CdRw => write!(f, "CD-RW"),
+            ReadCapabilities::DvdR => write!(f, "DVD-R"),
+            ReadCapabilities::DvdPlusR => write!(f, "DVD+R"),
+            ReadCapabilities::DvdRam => write!(f, "DVD-RAM"),
+            ReadCapabilities::DvdRom => write!(f, "DVD-ROM"),
+            ReadCapabilities::DvdRw => write!(f, "DVD-RW"),
+            ReadCapabilities::DvdPlusRw => write!(f, "DVD+RW"),
+            ReadCapabilities::C2Errors => write!(f, "C2 Errors"),
+            ReadCapabilities::Mode2Form1 => write!(f, "Mode 2 Form 1 (VCD)"),
+            ReadCapabilities::Mode2Form2 => write!(f, "Mode 2 Form 2 (VCD)"),
+            ReadCapabilities::Mcn => write!(f, "MCN"),
+            ReadCapabilities::Isrc => write!(f, "ISRC"),
+            _ => write!(f, "Unknown"),
+        }
+    }
+}
+impl fmt::Display for WriteCapabilities {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            WriteCapabilities::CdR => write!(f, "CD-R"),
+            WriteCapabilities::CdRw => write!(f, "CD-RW"),
+            WriteCapabilities::DvdR => write!(f, "DVD-R"),
+            WriteCapabilities::DvdPlusR => write!(f, "DVD+R"),
+            WriteCapabilities::DvdRam => write!(f, "DVD-RAM"),
+            WriteCapabilities::DvdRw => write!(f, "DVD-RW"),
+            WriteCapabilities::DvdPlusRw => write!(f, "DVD+RW"),
+            WriteCapabilities::Mrw => write!(f, "MRW"),
+            WriteCapabilities::BurnProof => write!(f, "Burn Proof"),
+            _ => write!(f, "Unknown"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -183,5 +353,11 @@ mod tests {
     #[ignore = "requires a disc drive"]
     fn hardware_info() {
         Drive::new().unwrap().hardware_info().unwrap();
+    }
+
+    #[test]
+    #[ignore = "requires a disc drive"]
+    fn capabilities() {
+        Drive::new().unwrap().capabilities().unwrap();
     }
 }
